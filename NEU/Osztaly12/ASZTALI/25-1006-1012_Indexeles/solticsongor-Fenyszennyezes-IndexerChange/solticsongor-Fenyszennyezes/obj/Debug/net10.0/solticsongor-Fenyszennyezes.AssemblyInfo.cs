6efac85293e38a646388cf2070df268dae162a35
Solti Csongor Péter <solti.csongor@akk.lan>
2026-09-15 10:50:11 +0200

    osztaly13 | Viragkoteszet

--- NEU/Osztaly12/ASZTALI/25-1006-1012_Indexeles/solticsongor-Fenyszennyezes-IndexerChange/solticsongor-Fenyszennyezes/obj/Debug/net10.0/solticsongor-Fenyszennyezes.AssemblyInfo.cs
+++ NEU/Osztaly12/ASZTALI/25-1006-1012_Indexeles/solticsongor-Fenyszennyezes-IndexerChange/solticsongor-Fenyszennyezes/obj/Debug/net10.0/solticsongor-Fenyszennyezes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("solticsongor-Fenyszennyezes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f851b2110594390b95bdc9bc7657849948b9249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca1d30ebc2e6bd41ac4c102038dfb0854bde869")]
 [assembly: System.Reflection.AssemblyProductAttribute("solticsongor-Fenyszennyezes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("solticsongor-Fenyszennyezes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
